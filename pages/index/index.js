2d6d0669ffc878ae7c32b2c437a792bd6645b9d6
p_zhengpan <p_zhengpan@tencent.com>
2021-08-05 11:57:30 +0800

    feat: Version upgrade UI adjustment

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,110 +1,45 @@
-// index.js
-// 获取应用实例
-const app = getApp()
-import { formatTime } from '../../utils/util'
-const QQMapWX = require('../../libs/qqmap-wx-jssdk.js')
-const qqmapsdk = new QQMapWX({
-  key: '6KHBZ-N3ZRU-KV4VG-BKRUE-VQT75-OQBZW'
-})
-
 Page({
+
+  /**
+   * 页面的初始数据
+   */
   data: {
-    hasUserInfo: false,
-    canIUseGetUserProfile: false,
-    // 用户信息
-    userInfo: {},
-    // 天气信息
+    cityId: null,
+    cityName: null,
     weatherInfo: {},
-    // 经纬度
-    latitude: null,
-    longitude: null
+    currentDate: new Date().toISOString().substr(0, 10)
   },
-  // 事件处理函数
-  onLoad() {
-    this.getLocation()
-    if (wx.getUserProfile) {
-      this.setData({
-        canIUseGetUserProfile: true
-      })
-    }
+  onShow: function () {
+    this.getWeatherInfo()
   },
-  onShow() {
-    this.getWeather()
-  },
-  getUserProfile(e) {
-    // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认
-    // 开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
-    wx.getUserProfile({
-      desc: '完善资料', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
-      success: (res) => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    })
-  },
-  getUserInfo(e) {
-    // 不推荐使用getUserInfo获取用户信息，预计自2021年4月13日起，getUserInfo将不再弹出弹窗，并直接返回匿名的用户个人信息
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
-  },
-  getLocation() {
-    wx.getLocation({
-      type: 'wgs84',
+  // 获取天气信息
+  getWeatherInfo() {
+    wx.getStorage({
+      key: 'cityInfo',
       success: res => {
-        const { latitude, longitude } = res
+        const { cityId, cityName } = res.data
         this.setData({
-          latitude,
-          longitude
+          cityId,
+          cityName
         })
-        qqmapsdk.reverseGeocoder({
-          location: {
-            latitude,
-            longitude
-          },
-          success: res => {}
-        })
-      }
-    })
-  },
-  getWeather() {
-    let cityId = '101280101'
-    let cityName = '广州'
-    try {
-      const cityInfo = wx.getStorageSync('cityInfo')
-      if (cityInfo) {
-        cityId = cityInfo.cityId
-        cityName = cityInfo.cityName
-      }
-    } catch (e) {}
-    
-    wx.request({
-      url: `https://devapi.qweather.com/v7/weather/now?key=baa77a1eac2e431e811b9982f6e6ccb1&location=${cityId}`,
-      success: (res) => {
-        const { code, now, updateTime } = res.data
-        if(code !== '200') {
-          wx.showToast({
-            title: code,
-          })
-          return
-        }
+      },
+      fail: err => {
         this.setData({
-          weatherInfo: {
-            ...now,
-            cityName,
-            obsTime: formatTime(new Date(now.obsTime))
+          cityId: '101280101',
+          cityName: '广州'
+        })
+      },
+      complete: () => {
+        wx.request({
+          url: `https://devapi.qweather.com/v7/weather/now?key=baa77a1eac2e431e811b9982f6e6ccb1&location=${this.data.cityId}`,
+          success: res => {
+            const { code, now, updateTime } = res.data
+            this.setData({
+              weatherInfo: now
+            })
           }
         })
       }
     })
   },
-  jumpSearch() {
-    const { latitude, longitude } = this.data
-    wx.navigateTo({
-      url: `/pages/search/search?latitude=${latitude}&longitude=${longitude}`,
-    })
-  }
 })
--- pages/search/search.js
+++ pages/search/search.js
@@ -1,87 +1,28 @@
-// pages/search/search.js
-const QQMapWX = require('../../libs/qqmap-wx-jssdk.js')
-let qqmapsdk = new QQMapWX({
-  key: '6KHBZ-N3ZRU-KV4VG-BKRUE-VQT75-OQBZW'
-});
-
+const cityList = require('../../utils/city')
 Page({
-  /**
-   * 页面的初始数据
-   */
   data: {
-    hotCity: [{
-      name: '北京',
-      key: 'beijing'
-    },{
-      name: '上海',
-      key: 'shanghai'
-    },{
-      name: '广州',
-      key: 'guangzhou'
-    },{
-      name: '深圳',
-      key: 'shenzhen'
-    },{
-      name: '杭州',
-      key: 'hangzhou'
-    },{
-      name: '西安',
-      key: 'xian'
-    },{
-      name: '重庆',
-      key: 'chongqing'
-    },{
-      name: '成都',
-      key: 'chengdu'
-    }],
-    longitude: null,
-    latitude: null,
-    markers: []
+    cityName: '',
+    resultList: []
   },
-  onLoad(options) {
+  onSearch(event) {
+    const cityName = event.detail
+    const resultList = cityList.filter(item => {
+      return item.cityZh.indexOf(cityName) !== -1
+    })
     this.setData({
-      ...options,
-      markers: [{
-        id: 1,
-        ...options
-      }]
+      cityName,
+      resultList
     })
   },
-  onShow(options) {
-    this.getCityList()
-  },
-  // 获取城市列表
-  getCityList() {
-    qqmapsdk.getCityList({
-      success: res => {},
-      fail: function(error) {
-        console.error(error);
-      }
+  onClickCell(event) {
+    const { action } = event.currentTarget.dataset
+    const { id:cityId, cityZh: cityName } = action
+    wx.setStorageSync('cityInfo',{
+      cityId,
+      cityName
     })
-  },
-  // 搜索事件
-  getWeather(e) {
-    const searchCity = e.detail.value ? e.detail.value : e.currentTarget.dataset.city
-    wx.request({
-      url: `https://geoapi.qweather.com/v2/city/lookup?key=baa77a1eac2e431e811b9982f6e6ccb1&location=${searchCity}`,
-      success: (res) => {
-        const { code, location } = res.data
-        if(code !== '200') {
-          wx.showToast({
-            title: code,
-          })
-          return
-        }
-        const { id: cityId, name: cityName } = location[0]
-        wx.setStorageSync('cityInfo', {
-          cityId,
-          cityName
-        })
-        wx.navigateBack({
-          delta: 1,
-        })
-      }
+    wx.navigateTo({
+      url: '../index/index',
     })
-    
   }
 })
